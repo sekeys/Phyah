a4d08c032ea83e2d268ed726127233d16b10d83e
sekeys <megamindrs@hotmail.com>
2017-03-28 12:18:04 +0800

    Update ChannelEmitter.cs

--- Phyah/Channel/ChannelEmitter.cs
+++ Phyah/Channel/ChannelEmitter.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace Phyah.Channel
 {
     using System;
@@ -14,24 +14,40 @@ namespace Phyah.Channel
         }
         public void Emit(ChannelEvent e)
         {
-            e.Executable.Execute();
+            foreach(var item in Emitter.Values)
+               Task.Run(()=>{ item.Accept(e);});
         }
 
         public void Emit<T>(ChannelEvent e)
         {
-
+            foreach(var item in Emitter.Values)
+                if(item is T)
+                    Task.Run(()=>{ item.Accept(e);});
         }
         public void Emit(string eventName,ChannelEvent e)
         {
-
+            if(Emitter.Contains(eventName)){
+                Emitter[eventName].Accept(e);
+            }
         }
         public ChannelEmitter  Subscribe(string name,ChannelReceptor receptor)
         {
+            if(Emitter.Contains(name)){
+                Emitter[name]=receptor;
+            }else{
+                Emitter.Add(name,receptor);
+            }
             return this;
         }
         public ChannelEmitter Subscribe(ChannelReceptor receptor)
         {
-            return this;
+            string str=nameof(Receptor);
+            if(str.EndsWith("ChannelReceptor")){
+                str=str.Substring(0,str.Length-15);
+            }else if(str.EndsWith("Receptor")){
+                str=str.Substring(0,str.Length-8);
+            }
+            return Subscribe(str,receptor);
         }
     }
 }
