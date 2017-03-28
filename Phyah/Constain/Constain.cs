ca3e9ca37345a600ac9054a711cf1261ab3eaf47
sekeys <megamindrs@hotmail.com>
2017-03-28 09:53:55 +0800

    Update Constain.cs

--- Phyah/Constain/Constain.cs
+++ Phyah/Constain/Constain.cs
@@ -9,19 +9,17 @@ namespace Phyah
         public const int SUCCESS = 200;
         public const int FAILD = 500;
         public const int NOTFOUND = 404;
-        public const int FORHIDDEN = 400;
-        public const int UNAUTHENTENICATION = 501;
+        public const int FORHIDDEN = 403;
+        public const int UNAUTHENTENICATION = 401;
         public const int UNKNOWERROR = 502;
-        public const int IGNOREERROR = 403;
-        public const int APPERROR = 402;
-        public const int CACHE = 301;
+        public const int IGNOREERROR = 405;
+        public const int APPERROR = 501;
         public const int REDIRECT = 300;
-        public const int UNLOGON = 504;
-        public const int NOTALLOW = 505;
+        public const int NOTALLOW = 405;
         public const int ANERROR = 506;
         public const int DEBUG = 199;
-        public const int DEBUGERROR = 499;
-
+        public const int DEBUGERROR = 502;
+        public const int BADREQUEST=400;
     }
     
 }
